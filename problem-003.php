a5d9ab6bc28b6da2383bde11127be755ec974024
Brian Lamb <fusani.applications@gmail.com>
2014-10-18 02:50:46 +0000

    Removing unsolved problems

--- problem-003.php
+++
@@ -1,9 +0,0 @@
-<?php
-// The prime factors of 13195 are 5, 7, 13 and 29.
-// What is the largest prime factor of the number 600851475143 ?
-
-require_once('models/Prime.php');
-
-$prime = new Prime();
-$primeFactors = $prime->factor(600851475143);
-echo max($primeFactors)."\n";
